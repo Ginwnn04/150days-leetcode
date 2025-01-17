91b504cf7f9ae2ed5acf8a2171ab1f7251e682b9
Ginwnn04 <quangdeeptry1911@gmail.com>
2025-01-18 00:13:19 +0700

    17/1

--- 977.squares-of-a-sorted-array.java
+++ 977.squares-of-a-sorted-array.java
@@ -10,42 +10,35 @@ class Solution {
         int[] result = new int[nums.length];
         int k = 0;
         int positive = 0;
-        int negative = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= 0) {
-                positive = i;
+        int negative = nums.length - 1;
+        while (negative >= 0 && positive < nums.length) {
+            if (nums[negative] >= 0)
+                negative--;
+            if (nums[positive] < 0)
+                positive++;
+            if (negative < 0 || positive >= nums.length)
                 break;
-            }
-        }
-        if (positive == 0) {
-            negative = nums.length;
-            positive = nums.length + 1;
-        }
-        
-        else if (positive == nums.length) {
-            negative = -1;
-            positive = 0;
+            if (nums[negative] < 0 && nums[positive] >= 0)
+                break;
         }
         while (negative >= 0 || positive < nums.length) {
             if (negative < 0) {
                 result[k++] = nums[positive] * nums[positive];
                 positive++;
-            } 
-            else if (positive > nums.length) {
+            } else if (positive >= nums.length) {
                 result[k++] = nums[negative] * nums[negative];
                 negative--;
-            }
-            else if (Math.abs(nums[negative]) >= Math.abs(nums[positive])) {
+
+            } else if (Math.abs(nums[negative]) >= Math.abs(nums[positive])) {
                 result[k++] = nums[positive] * nums[positive];
                 positive++;
             } else if (Math.abs(nums[negative]) < Math.abs(nums[positive])) {
                 result[k++] = nums[negative] * nums[negative];
                 negative--;
-            } 
+            }
         }
         return result;
-        
+
     }
 }
 // @lc code=end
-
